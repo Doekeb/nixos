7614598093b35281f0af5f836f9a6cc56956a066
doeke <doekebuursma@gmail.com>
2026-02-01 21:59:32 -0500

    Move hyprpaper installation to home manager.

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    hyprpaper
+  ];
   home.file.".config/hypr".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/dotfiles/.config/hypr";
 }
--- system/hyprland.nix
+++ system/hyprland.nix
@@ -6,10 +6,5 @@
       withUWSM = true;
       # wayland.windowManager.hyprland.systemd.enable = false; # Uncomment if using home manager
     };
-    # hyprlock.enable = true;
   };
-  environment.systemPackages = with pkgs; [
-    hyprpaper
-    # hyprshutdown
-  ];
 }
