dfa335a958cc3b605e4206f3008519cd1bc60018
doeke <doekebuursma@gmail.com>
2026-05-19 17:55:10 -0400

    Add wtype for auto-paste in noctalia clipboard manager.

--- home-manager/noctalia-shell.nix
+++ home-manager/noctalia-shell.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [ noctalia-shell ];
+  home.packages = with pkgs; [
+    noctalia-shell
+    wtype # auto-paste
+  ];
   home.file.".config/noctalia".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/dotfiles/.config/noctalia";
 }
