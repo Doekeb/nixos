5e48785a6ca2395e32987548195ace4af28f5f68
doeke <doekebuursma@gmail.com>
2026-05-31 16:00:01 -0400

    Add some extra python packages to the qgis environment.

--- home-manager/qgis.nix
+++ home-manager/qgis.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ qgis ];
+  home.packages = with pkgs; [
+    (qgis.override {
+      extraPythonPackages =
+        ps: with ps; [
+          numpy
+          geopandas
+          rasterio
+        ];
+    })
+    # other packages
+  ];
 }
