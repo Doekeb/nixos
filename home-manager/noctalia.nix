ff28ff44630b1f93ec6e567b16ada3b96395dfa5
doeke <doekebuursma@gmail.com>
2026-07-28 21:17:20 -0400

    Add back in the noctalia config symlink.

--- home-manager/noctalia.nix
+++ home-manager/noctalia.nix
@@ -1,10 +1,15 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
 {
   home.packages = [ inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default ];
   # home.packages = with pkgs; [
   #   noctalia-shell
   #   wtype # auto-paste
   # ];
-  # home.file.".config/noctalia".source =
-  #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/dotfiles/.config/noctalia";
+  home.file.".config/noctalia".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/dotfiles/.config/noctalia";
 }
