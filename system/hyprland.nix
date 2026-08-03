9a9f8b99ea88c51dcacbc9e14784569c79f0bb2b
doeke <doekebuursma@gmail.com>
2026-08-02 21:18:58 -0400

    Simplify hyprland installation.

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,17 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs = {
     hyprland = {
       enable = true;
       withUWSM = true;
-      # wayland.windowManager.hyprland.systemd.enable = false; # Uncomment if using home manager
     };
   };
-  environment.systemPackages = with pkgs; [
-    hyprpaper
-    hyprpicker
-    hyprpolkitagent
-    hyprpwcenter
-    hyprsysteminfo
-  ];
 }
