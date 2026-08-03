c75fc1f4e3a2fdc651f791600e51729e46b15f67
doeke <doekebuursma@gmail.com>
2026-08-02 22:08:49 -0400

    Trim up networking.

--- system/networking.nix
+++ system/networking.nix
@@ -1,18 +1,7 @@
 { ... }: {
   networking.hostName = "lemur-pro"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   # Needed for Noctalia network manager (and for networking to work in general)
   networking.networkmanager.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
