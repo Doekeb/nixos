8a23de664bde9439ced7b175afec365138b42ab4
doeke <doekebuursma@gmail.com>
2026-08-06 22:54:13 -0400

    Use nushell nixos module instead of package.

--- system/shells.nix
+++ system/shells.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ nushell ]; # Change to programs enable next flake update
+{ ... }: {
   programs = {
     bash.enable = true;
     fish.enable = true;
-    # nushell.enable = true;
+    nushell.enable = true;
     zsh.enable = true;
   };
 }
