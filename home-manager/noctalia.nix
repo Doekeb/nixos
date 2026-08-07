fa14019e669c6465eee0b47bbef23e5d5199237d
doeke <doekebuursma@gmail.com>
2026-08-06 23:04:49 -0400

    Add adw-gtk3 to let noctalia theme apps.

--- home-manager/noctalia.nix
+++ home-manager/noctalia.nix
@@ -1,5 +1,7 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  # Need adw-gtk3 to enable Noctalia to theme gtk3 apps
+  home.packages = with pkgs; [ adw-gtk3 ];
   xdg.configFile."noctalia".source =
     config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nixos/dotfiles/.config/noctalia";
 }
