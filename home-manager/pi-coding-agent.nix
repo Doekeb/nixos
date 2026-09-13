6036cbad4954012b8ca978c2698218045930e10e
doeke <doekebuursma@gmail.com>
2026-09-13 11:57:20 -0400

    Give pi access to npm.

--- home-manager/pi-coding-agent.nix
+++ home-manager/pi-coding-agent.nix
@@ -1,5 +1,6 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   programs.pi-coding-agent.enable = true;
+  programs.pi-coding-agent.extraPackages = [ pkgs.nodejs ];
   home.file.".pi/agent/settings.json".source =
     config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nixos/dotfiles/.pi/agent/settings.json";
 }
